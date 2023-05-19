c87164c105790c6e919d5f883b0d7898fd00f160
adityaazad79 <adityaazad79@gmail.com>
2023-05-19 23:38:52 +0530

    new files added

---
+++ Q47_Reverse_String.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    void reverseString(vector<char> &s)
+    {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right)
+        {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+    }
+};
+
+int main()
+{
+    Solution solution;
+
+    vector<char> s = {'H', 'e', 'l', 'l', 'o'};
+    solution.reverseString(s);
+
+    for (char c : s)
+    {
+        cout << c;
+    }
+    cout << std::endl;
+
+    return 0;
+}
