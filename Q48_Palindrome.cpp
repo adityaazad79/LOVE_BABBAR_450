87d57b15ad1518dd13cfabfe15bce8249f0a8110
adityaazad79 <adityaazad79@gmail.com>
2023-05-21 23:41:52 +0530

    new files added

---
+++ Q48_Palindrome.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+class Solution
+{
+public:
+    int isPalindrome(string S)
+    {
+        // Your code goes here
+        int len = S.length();
+        // cout << len << endl;
+        for (int i = 0; i < len / 2; i++)
+            if (S[i] != S[len - 1 - i])
+                return 0;
+        return 1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        Solution ob;
+
+        cout << ob.isPalindrome(s) << "\n";
+    }
+}
